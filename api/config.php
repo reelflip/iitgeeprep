84543029db04a1b36c0a33ed8f52667f64f0b117
reelflip <36274376+reelflip@users.noreply.github.com>
2025-12-20 10:59:48 +0530

    Update config.php

--- api/config.php
+++ api/config.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost";
-$db_name = "u123456789_iitjee";
-$user = "u123456789_admin";
-$pass = "";
+$db_name = "u131922718_iitgeedb2";
+$username = "u131922718_iitgeedb2_user";
+$password = "i2ZRkC7/dR";
 try {
     $conn = new PDO("mysql:host=$host;dbname=$db_name;charset=utf8mb4", $user, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
